764c9a946ab7bf9ba57121c9576bb14ffcdfe8d8
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-06-10 16:22:39 -0600

    Worked on the computers component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,11 +13,16 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText}`}>
+          <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Joshua</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop user interfaces <br className="sm:block hidden" />
+            for blockchain applications.
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from "react";
+// empty canvas allowing us to place something on it
+import { Canvas } from "@react-three/fiber";
+// adding controls that help us use the 3D tooling
+// use GLTF allows us to import 3D models
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  return <div>Computers</div>;
+};
 
-export default Computers
+export default Computers;
