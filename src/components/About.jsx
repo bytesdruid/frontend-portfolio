9b0cf4b2abd60fceb2987f354d05eae52ca4afec
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-06-11 16:10:37 -0600

    worked on about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}>
+        I am a frontend developer in the blockchain space. Experienced with
+        Typescript and Javascript.
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,7 +7,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   // instantiating the gltf file
   const computer = useGLTF("./desktop_pc/scene.gltf");
   // we use a mesh when implementing 3D models
@@ -26,8 +26,8 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -37,15 +37,23 @@ const Computers = () => {
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
+  // this changes the is mobile variable if the screen size is less than 500 px
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500)");
-
+    // add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    // sets initial value of the isMobile state variable
     setIsMobile(mediaQuery.matches);
-
+    // defines a call back function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
-  }, [third]);
+    // add callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    // remove listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <Canvas
@@ -62,7 +70,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
