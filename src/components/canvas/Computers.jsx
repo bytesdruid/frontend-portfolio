e3927bc13050a6039be0af4742944b986b026ff6
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-06-11 09:13:37 -0600

    added attributes to spot light

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -16,6 +16,14 @@ const Computers = () => {
       {/* the light allows us to see */}
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
