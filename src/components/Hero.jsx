f7b5b2f843f8d1296f7648e5f3dea41e7731941f
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2023-06-11 14:02:26 -0600

    added jumping circle using framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,8 +28,15 @@ const Hero = () => {
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
+                // when the animation occurs we move it 24 px up and down
                 y: [0, 24, 0],
               }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
